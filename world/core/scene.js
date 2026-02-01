56740993c0a2b78fb5db7a1d5e2d7425d6993c25
Martí Dalmases <martidalmases@gmail.com>
2026-02-01 23:00:44 +0100

    Update scene.js

--- world/core/scene.js
+++ world/core/scene.js
@@ -3,7 +3,7 @@ import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js";
 export function createScene() {
   const skyGeo = new THREE.SphereGeometry(1000, 64, 64);
   const skyMat = new THREE.MeshBasicMaterial({
-    color: 0x000011, // dark night base
+    color: 0xffffff, // dark night base
     side: THREE.BackSide // render inside of sphere
   });
 
