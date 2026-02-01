fe365d6f935e1b5fafa14ee7ee303216fd93d68a
Martí Dalmases <martidalmases@gmail.com>
2026-02-01 21:11:10 +0100

    Update main.js

--- world/main.js
+++ world/main.js
@@ -2,21 +2,46 @@ import { createRenderer } from "./core/renderer.js";
 import { createScene } from "./core/scene.js";
 import { createCamera, updateCamera } from "./core/camera.js";
 
+import {
+  createStars,
+  updateStars,
+  handleStarClick
+} from "./systems/stars.js";
+
+
 const scene = createScene();
 const camera = createCamera();
 const renderer = createRenderer();
 
+
+// Stars
+createStars(scene, 12);
+
+
+// Resize
 window.addEventListener("resize", () => {
+
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
+
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
 
+// Click handler
+window.addEventListener("click", () => {
+  handleStarClick();
+});
+
+
+// Loop
 function animate() {
+
   requestAnimationFrame(animate);
 
   updateCamera();
+  updateStars(camera);
+
   renderer.render(scene, camera);
 }
 
