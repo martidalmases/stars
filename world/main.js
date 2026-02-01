60aa2ffaccb8fa60b8e7988173589642e05e6a38
Martí Dalmases <martidalmases@gmail.com>
2026-02-01 20:02:06 +0100

    Update main.js

--- world/main.js
+++ world/main.js
@@ -1,1 +1,29 @@
+import { createRenderer } from "./core/renderer.js";
+import { createScene } from "./core/scene.js";
+import { createCamera, updateCamera } from "./core/camera.js";
+
+// Core
+const scene = createScene();
+const camera = createCamera();
+const renderer = createRenderer();
+
+// Resize
+window.addEventListener("resize", onResize);
+
+function onResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+// Animation loop
+function animate() {
+  requestAnimationFrame(animate);
+
+  updateCamera();
+  renderer.render(scene, camera);
+}
+
+animate();
 
