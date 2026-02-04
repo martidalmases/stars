57f12db343dd04bee1502ea8147f937a738e7d0c
Martí Dalmases <martidalmases@gmail.com>
2026-02-04 12:14:36 +0100

    Restart renderer.js

--- world/core/renderer.js
+++ world/core/renderer.js
@@ -1,19 +1,1 @@
-import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js";
-
-export function createRenderer() {
-
-  const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    alpha: false
-  });
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setPixelRatio(window.devicePixelRatio);
-
-  renderer.outputColorSpace = THREE.SRGBColorSpace;
-
-  document.body.appendChild(renderer.domElement);
-
-  return renderer;
-}
 
