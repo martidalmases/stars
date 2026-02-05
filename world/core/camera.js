20ef5812c86042552e5cefd6397400127546b3ad
Martí Dalmases <martidalmases@gmail.com>
2026-02-05 12:45:45 +0100

    Update camera.js

--- world/core/camera.js
+++ world/core/camera.js
@@ -1,1 +1,42 @@
+import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js";
 
+
+export function createCamera() {
+const camera = new THREE.PerspectiveCamera(
+70,
+window.innerWidth / window.innerHeight,
+0.1,
+2000
+);
+
+
+camera.position.set(0, 0, 0);
+
+
+return camera;
+}
+
+
+export function createDreamyController(camera) {
+let target = new THREE.Vector2();
+let current = new THREE.Vector2();
+
+
+const sensitivity = 0.0004;
+const smoothness = 0.05;
+
+
+window.addEventListener("mousemove", (e) => {
+target.x += e.movementY * sensitivity;
+target.y += e.movementX * sensitivity;
+});
+
+
+return function updateCamera() {
+current.lerp(target, smoothness);
+
+
+camera.rotation.x = current.x;
+camera.rotation.y = current.y;
+};
+}
