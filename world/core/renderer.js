f740d39406860eb89decd4a82600469026d1fd44
Martí Dalmases <martidalmases@gmail.com>
2026-02-05 12:46:30 +0100

    Update renderer.js

--- world/core/renderer.js
+++ world/core/renderer.js
@@ -1,1 +1,13 @@
+import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js";
 
+
+export function createRenderer() {
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+
+return renderer;
+}
